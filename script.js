2cd86b541c380f040934cd7a7d08764949895299
Lav <lavinia.blasi.o@gmail.com>
2024-08-27 17:16:49 +0200

    Modificato il testo del risultato del numero, adesso mostra direttamente il numero senza la frase.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('generaBtn').addEventListener('click', function() {
     const numero = Math.floor(Math.random() * 6) + 1;
-    document.getElementById('numeroGenerato').textContent = 'Il numero generato: ' + numero;
+    document.getElementById('numeroGenerato').textContent = numero;
 });

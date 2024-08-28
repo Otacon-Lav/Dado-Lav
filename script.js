f708b845d194e90b250a786aa3ab78991e810e94
Lav <lavinia.blasi.o@gmail.com>
2024-08-28 17:44:36 +0200

    aggiunto bottone per dark mode

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ document.getElementById('generaBtn').addEventListener('click', function() {
     const numero = Math.floor(Math.random() * 6) + 1;
     document.getElementById('numeroGenerato').textContent = numero;
 });
+
+// funzione per attivare/diattivare dark mode
+document.getElementById('darkModeToggle').addEventListener('click', function(){
+    document.body.classList.toggle('dark-mode');
+
+});
